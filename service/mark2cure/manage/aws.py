088251f6dac3080fd12985833fa9dc6e85856efd
Max Nanis <max@maxnanis.com>
2013-10-10 17:44:12 -0700

    Turk class init for persistent connection

--- service/mark2cure/manage/aws.py
+++ service/mark2cure/manage/aws.py
@@ -16,27 +16,24 @@ import requests, datetime
 class Turk(Command):
   "Add a mturk to a document"
 
-  # Utility Methods
-  def make_connection(self):
-      return MTurkConnection( aws_access_key_id = AWS_ACCESS_ID,
+  def __init__(self):
+    self.mtc = MTurkConnection( aws_access_key_id = AWS_ACCESS_ID,
                               aws_secret_access_key = AWS_SECRET_KEY,
                               host = AWS_HOST)
 
+  # Utility Methods
   def delete_all(self):
-      mtc = self.make_connection()
-      for hit in mtc.get_all_hits():
-          mtc.disable_hit( hit.HITId )
+      for hit in self.mtc.get_all_hits():
+          self.mtc.disable_hit( hit.HITId )
 
   def ban_user(self, worker_id, reason="Did not meet our standards."):
-      mtc = self.make_connection()
-      mtc.block_worker(worker_id, reason)
+      self.mtc.block_worker(worker_id, reason)
 
   def external_question(self, doc_id):
       # Would be cool to pull the length of the document to know the correct size of the window to show
       return ExternalQuestion("https://mark2cure.org/mturk/#/document/"+str(doc_id), 800)
 
   def make_qualification_test(self):
-      mtc = self.make_connection()
       name = 'Disease Recognitiion'
       description = 'Select the correct answer to move on'
       status = 'Active'
@@ -61,7 +58,7 @@ class Turk(Command):
       question_form.append(overview)
       question_form.append(q1)
 
-      qual_test = mtc.create_qualification_type(
+      qual_test = self.mtc.create_qualification_type(
                               name,
                               description,
                               status, keywords=None,
@@ -82,13 +79,15 @@ class Turk(Command):
         auto_granted = True,
         auto_granted_value = 2)
 
+  # get_qualification_score(qualification_type_id, worker_id)
+  # def get_created_qualifications
+
   # Actionable methods
   def hit_for_document(self, doc_id, max_assignments = 5, reward = 0.04, minutes = 8, title="Annotate scientific articles"):
-      mtc = self.make_connection()
       description = ('Visit a website and highlight diseases that are present in a paragraph.')
       keywords = 'science, annotation, disease'
 
-      hit = mtc.create_hit(
+      hit = self.mtc.create_hit(
           hit_type = None,
           question = self.external_question(doc_id),
           hit_layout = None,
