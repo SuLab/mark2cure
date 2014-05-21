98078768bf0d4e1988a1a65e408a7ee963b43221
Max Nanis <max@maxnanis.com>
2014-05-21 16:49:34 -0700

    Correcting Activity count for documents count values based on this experiment
    - Notify us when there are only a few documents left for work so we can close all HITs as soon as they're complete to prevent paying people only to train

--- mark2cure/common/utils.py
+++ mark2cure/common/utils.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models import Count
+from django.core.mail import send_mail
 
 from mark2cure.document.models import Activity
 
@@ -37,7 +38,14 @@ def experiment_routing(user, n_count, k_max = 5):
     for x in prev_docs:
         if x in experiment_docs: experiment_docs.remove(x)
 
-    activities = Activity.objects.filter(document__pk__in=experiment_docs, task_type='cr', submission_type='gm', experiment=None).values('document').annotate(Count('document'))
+    '''
+      Count the number of times the experimental docs have been completed for Concept Recognition
+      within this Experiment (if relevant), that aren't GM (none should be anyway)
+    '''
+    activities = Activity.objects.filter(
+        document__pk__in=experiment_docs,
+        task_type='cr',
+        experiment= settings.EXPERIMENT if user.userprofile.mturk else None).exclude(submission_type='gm').values('document').annotate(Count('document'))
     experiment_docs_completed = [item['document'] for item in activities if item['document__count'] >= k_max]
 
     for x in experiment_docs_completed:
@@ -46,6 +54,13 @@ def experiment_routing(user, n_count, k_max = 5):
     logger.debug("Experiment Docs remaining count {0} for {1} in experiment {2}".format(len(experiment_docs), user.username, settings.EXPERIMENT))
 
     random.shuffle(experiment_docs)
+
+    if len(experiment_docs) < 5:
+        send_mail('[Mark2Cure] Experiment Doc Remaining #{0}'.format(settings.EXPERIMENT),
+                'There are only {0} documents that have work left to be done.'.format(len(experiment_docs)),
+                settings.SERVER_EMAIL,
+                [email[1] for email in settings.MANAGERS])
+
     if len(experiment_docs) == 0:
         # All the docs have been completed (allow unlimited HITs with extra protection)
         return False
