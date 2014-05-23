5e2904af2521d5790caf916b75131d7c854bef04
Max Nanis <max@maxnanis.com>
2014-05-23 16:36:29 -0700

    More milestones for logging

--- mark2cure/common/views.py
+++ mark2cure/common/views.py
@@ -97,7 +97,7 @@ def mturk(request):
     if hit_index >= (len(gm_dict) + len(settings.EXPERIMENT_DOCS)):
         return render_to_response('common/nohits.jade', {'user_profile': user_profile }, context_instance=RequestContext(request))
 
-    if hit_index in [4,10,15]:
+    if hit_index in [4,5,6,10,15]:
         send_mail('[Mark2Cure] User Milestone',
                 'User {0} is on the flow, currently at document index {1}'.format(user.pk, hit_index),
                 settings.SERVER_EMAIL,
