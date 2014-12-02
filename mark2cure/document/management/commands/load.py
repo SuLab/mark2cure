efbab7c617089977d4ce8bc439c5ea178564e57b
Max Nanis <max@maxnanis.com>
2014-12-01 18:15:27 -0800

    Full task loading for Exper1
    - Document --documents & --assign
    - Static helpers for templates
    - TOS PDF
    - Fixtures update

--- mark2cure/document/management/commands/load.py
+++ mark2cure/document/management/commands/load.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from optparse import make_option
 
 from mark2cure.document.models import Document, Section
@@ -9,34 +9,32 @@ import csv
 
 
 class Command(BaseCommand):
-    help = 'Load the initial documents into the database associate them with quests'
+    help = 'Utils for populating the system with Documents & Annotations'
 
     option_list = BaseCommand.option_list + (
         make_option('--documents',
-        action='store_true',
-        dest='quest',
-        default=False,
-        help='Load the Documents into Mark2Cure and require 0 associations'),
-    )
+            action='store_true',
+            dest='documents',
+            default=False,
+            help='Load the Documents into Mark2Cure and require 0 associations'),
 
-    option_list = BaseCommand.option_list + (
         make_option('--assign',
-        action='store_true',
-        dest='quest',
-        default=False,
-        help='Bin the Documents into associated Quests'),
+            action='store_true',
+            dest='assign',
+            default=False,
+            help='Bin the Documents into associated Quests'),
+
+        make_option('--gm_anns',
+            action='store_true',
+            dest='gm_anns',
+            default=False,
+            help='Load the Documents into Mark2Cure and require 0 associations'),
     )
 
-    option_list = BaseCommand.option_list + (
-        make_option('--gm_anns',
-        action='store_true',
-        dest='quest',
-        default=False,
-        help='Load the Documents into Mark2Cure and require 0 associations'),
-    )
 
     def handle(self, *args, **options):
-        dataset = 'NCBI_corpus_testing_cleaned'
+        print args, options
+        datasets = ['NCBI_corpus_testing', 'NCBI_corpus_training', 'NCBI_corpus_development']
 
         '''
             Import all the annotations for our
@@ -85,43 +83,78 @@ class Command(BaseCommand):
                     except DoesNotExist:
                         doc = None
 
-
-
         '''
             Randomly assign the loaded documents into our bins
             ensure the 4 training always go into Quest 1
         '''
         if options['assign']:
-            documents = list(Document.objects.filter(source=dataset).values_list('id', flat=True))
-            smallest_bin = 3
-            largest_bin = 10
-            random.shuffle(documents)
-            quest_id = 3
+            gm_documents_ids = [3464560, 7759075, 8198128, 3591825];
+            gm_documents = Document.objects.filter(source='NCBI_corpus_training', document_id__in=gm_documents_ids).all()
+            task_counter = 1
 
-            while len(documents) > smallest_bin:
+            # GM hidden
+            task, task_created = Task.objects.get_or_create(
+                    name=str(task_counter),
+                    completions=None,
+                    requires_qualification=3,
+                    provides_qualification=4,
+                    points=5000)
+
+            for doc in task.documents.all():
+                dqr = DocumentQuestRelationship.objects.get(document=doc, task=task)
+                dqr.delete()
+
+            for gold_document in gm_documents:
+                DocumentQuestRelationship.objects.create(task=task, document=gold_document)
+
+            # Insert the other Documents
+            document_set = list(Document.objects.filter(source='NCBI_corpus_training').exclude(document_id__in=gm_documents_ids).values_list('id', flat=True))
+
+            smallest_bin = 16
+            largest_bin = 24
+            random.shuffle(document_set)
+
+            while len(document_set) > smallest_bin:
+                task_counter += 1
+                task, task_created = Task.objects.get_or_create(
+                        name=str(task_counter),
+                        completions=10,
+                        requires_qualification=4,
+                        provides_qualification=4,
+                        points=5000)
+
                 quest_size = int(random.uniform(smallest_bin, largest_bin))
-                sel = documents[0:quest_size]
+                sel = document_set[0:quest_size]
 
-                for i in sel: documents.remove(i)
-                print "Adding", len(sel), "to Quest:", quest_id
-                task = Task.objects.get(pk=quest_id)
+                for i in sel: document_set.remove(i)
+                print "Adding", len(sel), "to Quest:", task_counter
+
                 for doc in task.documents.all():
                     dqr = DocumentQuestRelationship.objects.get(document=doc, task=task)
                     dqr.delete()
 
-                for i in sel:
-                    document = Document.objects.get(pk=i)
+                for doc_idx in sel:
+                    document = Document.objects.get(pk=doc_idx)
                     DocumentQuestRelationship.objects.create(task=task, document=document)
 
-                quest_id += 1
             else:
-                if len(documents) > 0:
-                    print "Adding", len(documents), "to Quest:", quest_id
-                    task = Task.objects.get(pk=quest_id)
-                    for doc in task.documents.all(): task.documents.remove(doc)
+                if len(document_set) > 0:
+                    task_counter += 1
+                    task, task_created = Task.objects.get_or_create(
+                            name=str(task_counter),
+                            completions=10,
+                            requires_qualification=4,
+                            provides_qualification=4,
+                            points=5000)
 
-                    for i in sel:
-                        document = Document.objects.get(pk=i)
+                    print "Adding", len(document_set), "to Quest:", task_counter
+
+                    for doc in task.documents.all():
+                        dqr = DocumentQuestRelationship.objects.get(document=doc, task=task)
+                        dqr.delete()
+
+                    for doc_idx in sel:
+                        document = Document.objects.get(pk=doc_idx)
                         DocumentQuestRelationship.objects.create(task=task, document=document)
 
 
@@ -130,20 +163,21 @@ class Command(BaseCommand):
             without performing any quest binning
         '''
         if options['documents']:
-            with open('assets/datasets/{dataset}.txt'.format(dataset=dataset), 'r') as f:
-                reader = csv.reader(f, delimiter='\t')
-                for num, title, text in reader:
-                    print title
-                    doc, doc_c = Document.objects.get_or_create(document_id=num)
-                    doc.title = title
-                    doc.source = dataset
-                    doc.save()
+            for dataset in datasets:
+                with open('assets/datasets/{dataset}_cleaned.txt'.format(dataset=dataset), 'r') as f:
+                    reader = csv.reader(f, delimiter='\t')
+                    for num, title, text in reader:
+                        print title
+                        doc, doc_c = Document.objects.get_or_create(document_id=num)
+                        doc.title = title
+                        doc.source = dataset
+                        doc.save()
 
-                    sec, sec_c = Section.objects.get_or_create(kind='t', document=doc)
-                    sec.text = title
-                    sec.save()
+                        sec, sec_c = Section.objects.get_or_create(kind='t', document=doc)
+                        sec.text = title
+                        sec.save()
 
-                    sec, sec_c = Section.objects.get_or_create(kind='a', document=doc)
-                    sec.text = text
-                    sec.save()
+                        sec, sec_c = Section.objects.get_or_create(kind='a', document=doc)
+                        sec.text = text
+                        sec.save()
 
