b978b0bf039bbc4d9864fe2f0d991ea9ae2c85fd
Max Nanis <max@maxnanis.com>
2015-01-23 15:59:22 -0800

    Protecting analysis pages / limit field exposure

--- mark2cure/analysis/views.py
+++ mark2cure/analysis/views.py
@@ -4,18 +4,21 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from mark2cure.common.models import Task, UserQuestRelationship
 
+from django.contrib.auth.decorators import login_required
+
 import csv
 
+@login_required
 def users_training(request):
     response = HttpResponse(content_type='text/html')
     #response['Content-Disposition'] = 'attachment; filename="users_training_completion.csv"'
     writer = csv.writer(response)
 
     training = Task.objects.filter(kind=Task.TRAINING).all()
-    writer.writerow(['username', 'email', 'T1', 'T2', 'T3', 'E1_Q1', 'E2_Q2'])
+    writer.writerow(['user_pk', 'T1', 'T2', 'T3', 'E1_Q1', 'E2_Q2'])
 
     for user in User.objects.all():
-        row = [user.username, user.email]
+        row = [user.pk]
         for t in training:
             row.append(UserQuestRelationship.objects.filter(task=t, user=user, completed=True).exists())
 
