0875ebc3e0a0241fcf7d73b9b95d224e6514f282
Max Nanis <max@maxnanis.com>
2015-02-11 12:00:48 -0800

    username route regex. Sentry #1398

--- mark2cure/userprofile/urls.py
+++ mark2cure/userprofile/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     url(r'^points/$', views.user_points, name='points'),
 
     # Public routes
-    url(r'^(?P<username>[-\w]+)/edit/', views.settings, name='settings'),
-    url(r'^(?P<username>[-\w]+)/', views.public_profile, name='public-profile'),
+    url(r'^(?P<username>[\w.@+-]+)/edit/', views.settings, name='settings'),
+    url(r'^(?P<username>[\w.@+-]+)/', views.public_profile, name='public-profile'),
 
 )
