e60f73210269e45f370ec8c5af398b3a37905895
Max Nanis <max@maxnanis.com>
2015-02-16 03:42:00 -0800

    Addresses #76 & Closes #62
    - Success message on dashboard
    - News to homepage
    - no click button
    - thicker border for login
    - etc...

--- mark2cure/common/views.py
+++ mark2cure/common/views.py
@@ -81,19 +81,22 @@ def dashboard(request):
 
     if user_completed_count == len(serializer.data):
         msg = '<p class="lead text-center">Congratulations! You have completed all quests available to you. Thank you for your participation in this experiment.</p>'
-        messages.info(request, msg + msg_footer, extra_tags='safe alert-success')
+        #messages.info(request, msg + msg_footer, extra_tags='safe alert-success')
 
     elif user_completed_count >= 1 and community_completed_count == query_set_count - 1:
         msg = '<p class="lead text-center">Thank you very much for your participation in the first experiment. The Mark2Cure community has completed all the quests available.</p>'
-        messages.info(request, msg + msg_footer, extra_tags='safe alert-info')
+        #messages.info(request, msg + msg_footer, extra_tags='safe alert-info')
 
     elif community_completed_count == query_set_count - 1:
         msg = '<p class="lead text-center">Thank you for joining Mark2Cure. The Mark2Cure community has completed all the quests available.</p>'
-        messages.info(request, msg + msg_footer, extra_tags='safe alert-warning')
+        #messages.info(request, msg + msg_footer, extra_tags='safe alert-warning')
 
     else:
         msg = '<p class="lead text-center">Click on one of the quest numbers below to start the quest. Your contributions are important so complete as many quests as you can.</p>'
-        messages.info(request, msg, extra_tags='safe alert-success')
+        #messages.info(request, msg, extra_tags='safe alert-success')
+
+    msg = '<p class="lead text-center">The first Mark2Cure annotation campaign has just finished! Congratulations, nearly 9,000 annotation tasks were completed in 3 weeks thanks to our amazing community of Mark2Curators! While we analyze the collected data and prepare for the next annotation campaign, please do your part by helping us to find other Mark2Curators. Thank you! - Team Mark2Cure'
+    messages.info(request, msg, extra_tags='safe alert-success')
 
     ctx = {'tasks': tasks,
            'welcome': welcome,
