9120197e84753ea56f2148f5991c10fac199f8e0
Max Nanis <max@maxnanis.com>
2015-05-13 09:52:26 -0700

    SupportMessage to Admin panel

--- mark2cure/common/admin.py
+++ mark2cure/common/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from mark2cure.common.models import Task, DocumentQuestRelationship
+from mark2cure.common.models import SkillBadge, PointsBadge, Group, Task, UserQuestRelationship, DocumentQuestRelationship, SupportMessage
 
+admin.site.register(SkillBadge)
+admin.site.register(PointsBadge)
+admin.site.register(Group)
 admin.site.register(Task)
+admin.site.register(UserQuestRelationship)
 admin.site.register(DocumentQuestRelationship)
+admin.site.register(SupportMessage)
