9d221283a1df38f2519a547c949c18d676e0ea3b
Max Nanis <max@maxnanis.com>
2015-05-13 10:09:55 -0700

    confirm merge answer keys

--- mark2cure/common/admin.py
+++ mark2cure/common/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 
-from mark2cure.common.models import SkillBadge, PointsBadge, Group, Task, UserQuestRelationship, DocumentQuestRelationship, SupportMessage
+from mark2cure.common.models import Group, Task, UserQuestRelationship, DocumentQuestRelationship, SupportMessage
 
-admin.site.register(SkillBadge)
-admin.site.register(PointsBadge)
 admin.site.register(Group)
 admin.site.register(Task)
 admin.site.register(UserQuestRelationship)
