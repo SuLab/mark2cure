29914143628487aa801778c2dc5f495c5be44491
Max Nanis <max@maxnanis.com>
2015-05-19 15:08:58 -0700

    [admin update]

--- mark2cure/document/admin.py
+++ mark2cure/document/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db import models
+from django.contrib.humanize.templatetags.humanize import naturalday
 
 from mark2cure.document.models import Document, Pubtator, Section, View, Annotation
 
@@ -10,8 +12,43 @@ class MyInlineModelOptions(admin.TabularInline):
     # define sortable_excludes
     sortable_excludes = ('authors',)
 
+class AnnotationAdmin(admin.ModelAdmin):
+    list_display = ('text', 'type', 'start',
+            'section', 'username', 'pmid',
+            'quest', 'group', 'time_ago')
+
+    readonly_fields = ('kind', 'type', 'text',
+            'start', 'created', 'view')
+
+    def section(self, obj):
+        if obj.view:
+            return obj.view.section.get_kind_display()
+
+    def username(self, obj):
+        if obj.view.user:
+            return obj.view.user.username
+
+    def pmid(self, obj):
+        if obj.view.section:
+            return obj.view.section.document.document_id
+
+    def quest(self, obj):
+        uqr = obj.view.userquestrelationship_set.first()
+        return uqr.task
+
+    def group(self, obj):
+        uqr = obj.view.userquestrelationship_set.first()
+        return uqr.task.group.name
+
+    def time_ago(self, obj):
+        return '{delta} ({full_time})'.format(
+                delta=naturalday(obj.created),
+                full_time=obj.created)
+
+    mymodel = models.ForeignKey(Annotation)
+
 admin.site.register(Document)
 admin.site.register(Pubtator)
 admin.site.register(Section)
 admin.site.register(View)
-admin.site.register(Annotation)
+admin.site.register(Annotation, AnnotationAdmin)
