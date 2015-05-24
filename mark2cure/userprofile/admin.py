258a7d1a8b133437f321c2209f6876472fbdefb3
Max Nanis <max@maxnanis.com>
2015-05-24 04:09:48 -0700

    Userprofile / Team Admin

--- mark2cure/userprofile/admin.py
+++ mark2cure/userprofile/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
+from django.db import models
 from .models import UserProfile, Team
 
 
-admin.site.register(Team)
-admin.site.register(UserProfile)
+class TeamAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'owner__username']
+    list_display = ['name', 'owner', 'created']
+
+    mymodel = models.ForeignKey(Team)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    search_fields = ['user__username', 'referral', 'motivation', 'quote']
+
+    list_display = ['user', 'team',
+            'email_notify', 'gender', 'age',
+            'occupation', 'education', 'science_education',
+            'country', 'referral', 'motivation',
+            'quote']
+
+    readonly_fields = ['user', 'team', 'rating',
+            'email_notify', 'gender', 'age',
+            'occupation', 'education', 'science_education',
+            'country', 'referral', 'motivation',
+            'quote']
+
+    mymodel = models.ForeignKey(UserProfile)
+
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
