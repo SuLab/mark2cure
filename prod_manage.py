fd84f8a6bb3cd2942b4b4ddd6a64d47d6e4e6056
Max Nanis <max@maxnanis.com>
2015-06-28 04:17:36 -0700

    Production manage util to simply migrations

---
+++ prod_manage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mark2cure.settings")
+    os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
+
+    from configurations.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
