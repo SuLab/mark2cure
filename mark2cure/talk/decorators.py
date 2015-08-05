8de7838628995b11fc9eb401faa3dc643a07c00e
Max Nanis <max@maxnanis.com>
2015-08-05 07:22:44 -0700

    Comment Moderators to Talk Decorator. Closes #51

--- mark2cure/talk/decorators.py
+++ mark2cure/talk/decorators.py
@@ -12,7 +12,7 @@ def doc_completion_required(function):
         # (TODO) This is horrendous
         doc_pk = Document.objects.filter(document_id=kwargs['pubmed_id']).values('pk')[0]['pk']
 
-        if doc_pk not in completed_document_pks:
+        if not request.user.groups.filter(name='Comment Moderators').exists() and doc_pk not in completed_document_pks:
             return HttpResponseForbidden('Access denied!')
         else:
             return function(request, *args, **kwargs)
