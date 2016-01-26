3f7321f38f2d627a511fae954e7927f2baa3d5f1
Max Nanis <max@maxnanis.com>
2016-01-26 10:45:48 -0800

    Safe Group template search (#f92a7ea implement)

--- mark2cure/common/templatetags/group.py
+++ mark2cure/common/templatetags/group.py
@@ -5,5 +5,4 @@ register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
-    group = Group.objects.get(name=group_name)
-    return True if group in user.groups.all() else False
+    return user.groups.filter(name=group_name).exists()
