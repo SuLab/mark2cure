3cc83bdce23e34fcc06dfb8c741ec0cf5bdd1596
Max Nanis <max@maxnanis.com>
2016-05-18 11:09:11 -0700

    [Instructions] Proper app organization

--- mark2cure/instructions/urls.py
+++ mark2cure/instructions/urls.py
@@ -1,24 +1,56 @@
 from django.conf.urls import patterns, url
-from . import views
+from django.views.generic import TemplateView
+
 
 urlpatterns = patterns('',
-    # Initial training for fresh signups
-    url(r'^disease-marking/$',
-        views.disease_marking, name='disease-marking'),
 
-    url(r'^gene-marking/$',
-        views.gene_marking, name='gene-marking'),
+    # Entity Recognition Task Specific
+    url(r'^entity-recognition/disease-marking/$',
+        TemplateView.as_view(template_name='instructions/tasks/entity-recognition/diseases.jade'),
+        name='entity-recognition-diseases'),
 
-    url(r'^treatment-marking/$',
-        views.treatment_marking, name='treatment-marking'),
+    url(r'^entity-recognition/gene-marking/$',
+        TemplateView.as_view(template_name='instructions/tasks/entity-recognition/genes.jade'),
+        name='entity-recognition-genes'),
 
-    url(r'^gene-disease-relation/$',
-        views.gene_disease_relation, name='gene-disease-relation'),
+    url(r'^entity-recognition/treatment-marking/$',
+        TemplateView.as_view(template_name='instructions/tasks/entity-recognition/treatments.jade'),
+        name='entity-recognition-treatments'),
 
-    url(r'^drug-disease-relation/$',
-        views.drug_disease_relation, name='drug-disease-relation'),
+    url(r'^entity-recognition/$',
+        TemplateView.as_view(template_name='instructions/tasks/entity-recognition/home.jade'),
+        name='entity-recognition'),
 
-    url(r'^gene-drug-relation/$',
-        views.gene_drug_relation, name='gene-drug-relation'),
+    # Relation Task Specific
+    url(r'^relation/definition/disease/$',
+        TemplateView.as_view(template_name='instructions/tasks/relation/disease-concept.jade'),
+        name='relation-definition-disease'),
+
+    url(r'^relation/definition/gene/$',
+        TemplateView.as_view(template_name='instructions/tasks/relation/gene-concept.jade'),
+        name='relation-definition-gene'),
+
+    url(r'^relation/definition/drug/$',
+        TemplateView.as_view(template_name='instructions/tasks/relation/drug-concept.jade'),
+        name='relation-definition-drug'),
+
+    url(r'^relation/gene-disease/$',
+        TemplateView.as_view(template_name='instructions/tasks/relation/gene-disease-relation.jade'),
+        name='relation-gene-disease'),
+
+    url(r'^relation/drug-disease/$',
+        TemplateView.as_view(template_name='instructions/tasks/relation/drug-disease-relation.jade'),
+        name='relation-drug-disease'),
+
+    url(r'^relation/gene-drug/$',
+        TemplateView.as_view(template_name='instructions/tasks/relation/gene-drug-relation.jade'),
+        name='relation-gene-drug'),
+
+    url(r'^relation/$',
+        TemplateView.as_view(template_name='instructions/tasks/relation/home.jade'),
+        name='relation'),
+
+
+    url(r'', TemplateView.as_view(template_name='instructions/home.jade'), name='home'),
 
 )
