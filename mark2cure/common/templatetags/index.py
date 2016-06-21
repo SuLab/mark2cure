deccfb1d39fa3fde3a256013792b03fbca912709
Jennifer Fouquier <jennietf@gmail.com>
2016-06-21 16:35:07 -0700

    index.py was only used temporarily; removed it

--- mark2cure/common/templatetags/index.py
+++
@@ -1,8 +0,0 @@
-# taken from http://stackoverflow.com/questions/4651172/reference-list-item-by-index-within-django-template/29664945#29664945
-
-from django import template
-register = template.Library()
-
-@register.filter
-def index(List, i):
-    return List[int(i)]
