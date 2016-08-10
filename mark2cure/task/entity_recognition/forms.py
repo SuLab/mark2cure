1bc2d43910e507571af3ad1c8c263e4d8384e9fe
Max Nanis <max@maxnanis.com>
2016-08-10 11:10:11 -0700

    [ER] Confirming Annotation start position relative

--- mark2cure/task/entity_recognition/forms.py
+++ mark2cure/task/entity_recognition/forms.py
@@ -3,6 +3,14 @@ from .models import EntityRecognitionAnnotation
 
 
 class EntityRecognitionAnnotationForm(forms.ModelForm):
+    '''
+        Responsible for saving POST'd data from the YPet
+        library.
+
+        - The start position that is given is relative to the
+        passage it's contained within, NOT the entire document
+    '''
+
     class Meta:
         model = EntityRecognitionAnnotation
         fields = ['text', 'start', 'type']
