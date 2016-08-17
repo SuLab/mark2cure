190b648c61ddf107fcaedacf90243a0a211cd9c6
Max Nanis <max@maxnanis.com>
2016-08-17 08:24:13 -0700

    [Document] Remove unused ‘read-specific-pubtator’

--- mark2cure/document/views.py
+++ mark2cure/document/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 
-from ..common.bioc import BioCReader, BioCWriter
 from ..common.formatter import bioc_as_json, clean_df, apply_annotations
 from .models import Document, Pubtator
 
@@ -23,28 +22,6 @@ def read_pubtator(request, pk):
     """Return the exact Pubtator file that was downloaded"""
     pubtator = get_object_or_404(Pubtator, pk=pk)
     return HttpResponse(pubtator.content, content_type='text/xml')
-
-
-def read_specific_pubtator_bioc(request, pub_pk, format_type):
-    # When fetching via pubmed, include no annotaitons
-    pubtator = get_object_or_404(Pubtator, pk=pub_pk)
-
-    r = BioCReader(source=pubtator.content)
-    r.read()
-
-    writer = BioCWriter()
-    writer.collection = r.collection
-
-    # writer = bioc_writer(request)
-    # bioc_document = doc.as_bioc_with_pubtator_annotations()
-    # writer.collection.add_document(bioc_document)
-    # return HttpResponse(200);
-
-    if format_type == 'json':
-        writer_json = bioc_as_json(writer)
-        return HttpResponse(writer_json, content_type='application/json')
-    else:
-        return HttpResponse(writer, content_type='text/xml')
 
 
 def read_pubtator_bioc(request, pubmed_id, format_type):
