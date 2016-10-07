823363b321c06a622c61dbcacd18ab269c7cf1d8
Max Nanis <max@maxnanis.com>
2016-10-06 17:35:49 -0700

    [authentication] password_hashers order bcrypt

--- mark2cure/settings.py
+++ mark2cure/settings.py
@@ -161,12 +161,10 @@ ROOT_URLCONF = 'mark2cure.urls'
 WSGI_APPLICATION = 'mark2cure.wsgi.application'
 
 PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.SHA1PasswordHasher',
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-    'django.contrib.auth.hashers.CryptPasswordHasher',
 )
 
 LANGUAGE_CODE = 'en-us'
