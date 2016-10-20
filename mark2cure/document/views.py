1bfe14fb5abc0498319dacca9e7bb4d5c1463c04
Max Nanis <max@maxnanis.com>
2016-10-20 14:16:05 -0700

    [minor] document cleanup / organization

--- mark2cure/document/views.py
+++ mark2cure/document/views.py
@@ -6,9 +6,9 @@ from .models import Document, Pubtator
 
 
 def read_bioc(request, pubmed_id, format_type):
-    '''
+    """
         A plain (no annotations) BioC file for a PMID
-    '''
+    """
     doc = get_object_or_404(Document, document_id=pubmed_id)
 
     writer = Document.objects.as_writer(documents=[doc])
@@ -20,19 +20,11 @@ def read_bioc(request, pubmed_id, format_type):
         return HttpResponse(writer, content_type='text/xml')
 
 
-def read_pubtator(request, pk):
-    '''
-        Return the exact Pubtator file that was downloaded
-    '''
-    pubtator = get_object_or_404(Pubtator, pk=pk)
-    return HttpResponse(pubtator.content, content_type='text/xml')
-
-
 def read_pubtator_bioc(request, pubmed_id, format_type):
-    '''
+    """
         A merged file of the multiple Pubtator responses
         Prevents overlap of text as this is used by YPet
-    '''
+    """
     # When fetching via pubmed, include no annotaitons
     doc = get_object_or_404(Document, document_id=pubmed_id)
 
@@ -47,4 +39,12 @@ def read_pubtator_bioc(request, pubmed_id, format_type):
         return HttpResponse(writer_json, content_type='application/json')
     else:
         return HttpResponse(writer, content_type='text/xml')
+
+def read_pubtator(request, pk):
+    """
+        Return the exact Pubtator file that was downloaded
+    """
+    pubtator = get_object_or_404(Pubtator, pk=pk)
+    return HttpResponse(pubtator.content, content_type='text/xml')
+
 
